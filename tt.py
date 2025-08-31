24c440c523cc1a1f33682cf47d593c66c812bf2e
amirsa <sataryamir@gmail.com>
2025-08-31 12:09:37 +0330

    tt.py

--- tt.py
+++ tt.py
@@ -11,7 +11,7 @@ from sklearn.neural_network import MLPRegressor
 # =============================
 # تنظیمات اپ
 # =============================
-file_url = "https://github.com/amirsatarirad/test/raw/refs/heads/main/tarmim3.xlsx" # لینک مورد نظر خود را اینجا قرار دهید
+file_url = "https://raw.githubusercontent.com/amirsatarirad/test/refs/heads/main/tarmim3.xlsx" # لینک مورد نظر خود را اینجا قرار دهید
 
 st.set_page_config(page_title="Modeling App", layout="wide")
 st.title("📊 Modeling App (SVR, MLP, Ensemble)")
@@ -85,6 +85,7 @@ else:
     st.write(f"**SVR Prediction:** {y_pred_svr:.3f}")
     st.write(f"**MLP Prediction:** {y_pred_mlp:.3f}")
     st.write(f"**SVR & MLP Ensemble Prediction:** {y_pred_ensemble:.3f}")
+
 
 
 
